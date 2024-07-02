5699fcb41f02915e1aa750dc2cbdfe100f2aeb73
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-07-02 11:13:44 +0700

    feat: [Learn Flutter] Create to-do items

---
+++ lib/widgets/todo_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+import '../model/todo.dart';
+
+class ToDoItem extends StatelessWidget {
+  final ToDo todo;
+
+  // ignore: use_super_parameters
+  const ToDoItem({Key? key, required this.todo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: ListTile(
+          onTap: () => print("Task 1"),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+          tileColor: Colors.white,
+          leading: Icon(
+              todo.isCompleted
+                  ? Icons.check_box
+                  : Icons.check_box_outline_blank,
+              color: tdBlue),
+          title: Text(todo.title,
+              style: TextStyle(
+                  color: tdBlack,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  decoration: todo.isCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none)),
+          trailing: Container(
+            padding: const EdgeInsets.all(0),
+            margin: const EdgeInsets.symmetric(vertical: 12),
+            decoration: BoxDecoration(
+              color: tdRed,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            height: 35,
+            width: 35,
+            child: IconButton(
+              icon: const Icon(Icons.delete, color: Colors.white),
+              iconSize: 18,
+              onPressed: () => {print("Delete Task 1")},
+            ),
+          )),
+    );
+  }
+}
